fc94f2429e18764bd629ff79c35de5d3b8ea065d
codertetianka <144258918+codertetianka@users.noreply.github.com>
2024-01-16 17:31:42 +0000

    Complete task 5

--- models/articles-model.js
+++ models/articles-model.js
@@ -9,3 +9,27 @@ exports.getArticleByIdModel = async (article_id) => {
   return rows[0];
 };
 
+exports.getAllArticlesModel = async () => {
+  const { rows } = await db.query(`
+    SELECT
+      articles.article_id,
+      articles.title,
+      articles.topic,
+      articles.author,
+      articles.created_at,
+      articles.votes,
+      articles.article_img_url,
+      COUNT(comments.article_id)::int AS comment_count
+    FROM articles
+    LEFT JOIN comments
+    ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC
+  `);
+
+  if (!rows.length) {
+    throw new Error('Not found');
+  }
+
+  return rows;
+};
